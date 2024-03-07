3741bc6be443e597f50381e16cf0b1bd9a928fdb
Jan Kandrac <jan.kandrac@ssnd.sk>
2024-03-07 08:32:46 +0100

    Init

---
+++ app/src/main/java/xyz/kandrac/oopcore/MainActivity.kt
@@ -0,0 +1,19 @@
+package xyz.kandrac.oopcore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LandingScreen()
+        }
+    }
+}
+
+@Composable
+fun LandingScreen() {
+}
